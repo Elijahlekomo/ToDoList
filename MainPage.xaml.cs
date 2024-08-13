877d4c63356db7e476c5d4994072e30e1553b85d
Elijah Lekomo <111081188+Elijahlekomo@users.noreply.github.com>
2024-08-13 05:46:16 +0200

    Add project files

---
+++ MainPage.xaml.cs
@@ -0,0 +1,13 @@
+﻿using To_Do_List.ViewModels;
+
+namespace To_Do_List;
+public partial class MainPage : ContentPage
+{       
+    public MainPage(MainPageViewModel vm)
+    {
+        InitializeComponent();
+        BindingContext = vm;
+    }
+
+    
+}
---
+++ ViewModels/MainPageViewModel.cs
@@ -0,0 +1,31 @@
+﻿using CommunityToolkit.Mvvm.ComponentModel;
+using CommunityToolkit.Mvvm.Input;
+using System.Collections.ObjectModel;
+//using UIKit;
+
+namespace To_Do_List.ViewModels;
+public partial class MainPageViewModel : ObservableObject
+{
+    public MainPageViewModel()
+    {
+        Items = new ObservableCollection<string>();
+    }
+
+    [ObservableProperty]
+    ObservableCollection<string> items;
+
+    [ObservableProperty]
+    string text;
+
+    [RelayCommand]
+    void Add()
+    {
+        Items.Add(Text);
+    }
+
+    [RelayCommand]
+    void Remove(string s) {
+     if (Items.Contains(s))
+        Items.Remove(s);
+    }
+}
